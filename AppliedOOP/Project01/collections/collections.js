484de05061ee3ed814a88c074257e361f3491e5d
Edglatus <tabrinath@gmail.com>
2019-10-15 05:27:26 -0300

    Updated views and layout on AppliedOOP/Project01

--- AppliedOOP/Project01/collections/collections.js
+++ AppliedOOP/Project01/collections/collections.js
@@ -8,8 +8,8 @@ Guests.insert({name: 'Eddy', extras: '2'})
 Guests.insert({name: 'Cake', extras: '3'})
 Guests.insert({name: 'Jeffy', extras: '1'})
 
-Parties.insert({name: 'The End', date: '17/12/2015'})
-Parties.insert({name: 'RPG', date: '31/12/5420'})
+Parties.insert({name: 'The End', date: '2015-12-17'})
+Parties.insert({name: 'RPG', date: '5420-12-31'})
 
 module.exports = {
 	Guests : database.getCollection('guests'),
